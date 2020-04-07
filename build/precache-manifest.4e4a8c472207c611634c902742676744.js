16717196054a1080d46f988b0d428fa3c3df488d
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-04-07 19:14:30 +0200

    commit final 2

---
+++ build/precache-manifest.4e4a8c472207c611634c902742676744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa48fda727fcc9e155ce4d842e3973e",
+    "url": "/Simulador/index.html"
+  },
+  {
+    "revision": "e06ef3e980186360c6a4",
+    "url": "/Simulador/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "653ff569ef0c390ff3ff",
+    "url": "/Simulador/static/js/2.00edb1a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simulador/static/js/2.00edb1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06ef3e980186360c6a4",
+    "url": "/Simulador/static/js/main.722d15e3.chunk.js"
+  },
+  {
+    "revision": "f1e6ffdfd33c05db2a85",
+    "url": "/Simulador/static/js/runtime-main.d3903c1a.js"
+  }
+]);
